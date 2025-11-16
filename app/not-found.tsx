78c23e01094b825347c23cf4ccf090a406f30c5d
stagepluspro-create <stagepluspro@gmail.com>
2025-11-16 18:44:30 +0200

    Updated not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -18,7 +18,7 @@ export default function NotFound() {
             Page Not Found
           </h2>
           <p className="text-gray-400">
-            The page you&apos;re looking for doesn&apos;t exist or has been moved.
+            The page you're looking for doesn't exist or has been moved.
           </p>
         </div>
 
