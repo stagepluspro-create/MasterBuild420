b0385f1eabbe7651535835b250331820bcada079
stagepluspro-create <stagepluspro@gmail.com>
2025-11-20 18:18:28 +0200

    Updated supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -2,11 +2,9 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/auth-helpers-nextjs";
 
 export const createServerSupabase = () => {
-  const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: cookieStore }
+    { cookies }
   );
 };
