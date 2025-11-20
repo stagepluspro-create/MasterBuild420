ad17a04ebd665a40f483e26e8f56b22cc4ad9be4
stagepluspro-create <stagepluspro@gmail.com>
2025-11-20 18:26:08 +0200

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,8 @@
-// lib/supabase.ts
 "use client";
 
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      flowType: "pkce",
-    },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
